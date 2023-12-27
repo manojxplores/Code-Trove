373afd83bc5f8f340dc7d7839d659d20d8d3b04a
ash9476 <manojsai.l2004@gmail.com>
2023-12-27 13:41:16 +0530

    Added JS basics

--- OdinProject/js_basics/variables.js
+++ OdinProject/js_basics/variables.js
@@ -67,39 +67,3 @@ console.log(text)
 
 
 
-// OPERATORS
-/* ARITHMETIC OPERATORS( +, -, *, %, **, /)
-   ASSIGNMENT OPERATORS( %=, +=, -=, *= , /=...)
-   COMPARISION OPERATORS( ==, !+, >, <, >= , <=, ?)
-   LOGICAL OPERATORS(&&, ||, !)
-
-*/
-
-let text1 = "John "
-let text2 = "Doe"
-console.log("Good Morning " + text1 + text2)
-
-// DATA TYPES
-
-// JS is a dynamically typed language 
-// same variable can hold mulitple data types
-let counter = 120
-console.log(typeof counter)
-counter = false
-console.log(typeof counter)
-
-let counter2; 
-console.log(typeof counter2)
-
-
-const cars = ['volvo', 'bmw', 'ferrari']
-// we can access the array items using index
-console.log(cars[0])
-
-const person1 = {
-    firstName : 'ash',
-    lastName : 'Ketchum',
-    age : 18
-}
-
-
