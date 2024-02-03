2526c54a37dc279740a8a6aba06174d97f43d00c
ash9476 <manojsai.l2004@gmail.com>
2024-02-03 16:18:24 +0530

    Add toggleReadStatus function

--- webDev/library/script.js
+++ webDev/library/script.js
@@ -1,4 +1,4 @@
-function Book(title, author, status){
+function Book(title, author, status) {
     this.title = title;
     this.author = author;
     this.status = status;
@@ -10,38 +10,42 @@ let bookCounter = 0;
 function addBook(book) {
     const main = document.getElementById('main');
     const container = document.createElement('div');
-    container.classList.add('container'); 
+    container.classList.add('container');
     const ul = document.createElement('ul');
 
     for (const key in book) {
         const li = document.createElement('li');
-        li.textContent = `${book[key]}`;
+        li.textContent = `${book[key]}`; // Display key-value pairs
         ul.appendChild(li);
     }
 
-    const edit_container = document.createElement('div');
-    edit_container.classList.add(`${bookCounter++}`);
-    
-    
     const removeBtn = document.createElement('button');
     removeBtn.textContent = 'Remove';
-    removeBtn.addEventListener('click', function() {
+    removeBtn.addEventListener('click', function () {
         removeBook(book);
     });
 
-    edit_container.appendChild(removeBtn);
+    const edit_status = document.createElement('button');
+    edit_status.textContent = 'Edit Status'
+    edit_status.addEventListener('click', function () {
+        book.status = book.status === 'Read' ? 'Not Yet' : 'Read';
+        refreshLibrary();
+    });
+
     container.appendChild(ul);
-    container.appendChild(edit_container);
+    container.appendChild(removeBtn);
+    container.appendChild(edit_status);
     main.appendChild(container);
 }
 
-function newBook(){
+function newBook() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const status = document.getElementById('status').value;
     const newBookObj = new Book(title, author, status);
     myLibrary.push(newBookObj);
     addBook(newBookObj);
+    console.log(myLibrary);
 }
 
 function removeBook(book) {
@@ -54,12 +58,12 @@ function removeBook(book) {
 
 function refreshLibrary() {
     const main = document.getElementById('main');
-    main.innerHTML = ''; 
-
+    main.innerHTML = '';
     myLibrary.forEach(addBook);
+    console.log(myLibrary);
 }
 
 const new_btn = document.getElementById('btn');
-new_btn.addEventListener('click', function(){
+new_btn.addEventListener('click', function () {
     newBook();
 });
